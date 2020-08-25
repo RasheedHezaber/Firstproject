b045f110956300b39f4d3d2e057d32498bdea1cf
RasheedHezaber <rasheedhezaber@gmail.com>
2020-08-26 01:56:09 +0300

    first commit

---
+++ src/Typelntro.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    experiencPoints+=5;
+    println(experiencPoints)
+    println(playerName)
+    var magic= playerName.reversed();
+    println(magic)
+}
+var experiencPoints=5;
+val playerName = "Estragon";
+var hasSteed=1
+
